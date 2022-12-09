066f5bc388f53088bc459892e911b358d2db0803
Tom Steele <tsteele@madisoncollege.edu>
2022-12-08 22:30:13 -0600

    Remembering concat as way to work toward expand...

--- src/app/swapi.service.ts
+++ src/app/swapi.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { EMPTY, expand, repeat } from 'rxjs';
+import { EMPTY, expand, repeat, concat } from 'rxjs';
 
 export interface SwapiPlanetShape {
   next: string;
@@ -18,5 +18,10 @@ export class SwapiService {
       // repeat(6)
       expand(x => x.next ? this.httpSvc.get<SwapiPlanetShape>(x.next) : EMPTY)
     );  
+
+    // const p1 = this.httpSvc.get<SwapiPlanetShape>("https://swapi.dev/api/planets");
+    // const p2 = this.httpSvc.get<SwapiPlanetShape>("https://swapi.dev/api/planets/?page=2");
+     
+    // return concat(p1, p2);
   };
 }
