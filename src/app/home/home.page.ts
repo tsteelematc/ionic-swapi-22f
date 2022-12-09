5b92304255c63adcdc9e5ff57c151d1b95a4c982
Tom Steele <tsteele@madisoncollege.edu>
2022-12-08 20:02:50 -0600

    Showing real planets...
    With fake repeat() for 60...

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SwapiService } from '../swapi.service';
 
 @Component({
@@ -6,8 +6,28 @@ import { SwapiService } from '../swapi.service';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
   constructor(private swapiSvc: SwapiService) {}
 
+  planets: string[] = [];
+
+  ngOnInit() {
+    this.swapiSvc
+      .loadPlanets()
+      .subscribe({
+         next: (data) => {
+          console.log(data);
+          this.planets = [
+            ...this.planets
+            , ...(data.results.map(x => x.name))
+          ].sort();
+          console.log(this.planets);
+         }
+        , error: (err) => {
+          console.error(err);
+        }
+      })
+    ;
+  }
 }
--- src/app/swapi.service.ts
+++ src/app/swapi.service.ts
@@ -1,10 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { repeat } from 'rxjs';
 
+export interface SwapiPlanetShape {
+  next: string;
+  results: {name: string}[];
+}
 @Injectable({
   providedIn: 'root'
 })
 export class SwapiService {
 
   constructor(private httpSvc: HttpClient) { }
+
+  loadPlanets = () => {
+    return this.httpSvc.get<SwapiPlanetShape>("https://swapi.dev/api/planets").pipe(
+      repeat(6)
+    );  
+  };
 }
